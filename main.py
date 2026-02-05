330681c6f95c1c98e4ea56c84fc5373c0ecbb464
GogAndMagog <sergejsmel6@gmail.com>
2026-02-05 16:59:14 +0300

    feat: add arguments parsing for main script

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import argparse
+
 from anki_genearor import  DeckGenerator
 from toml_loader import TomlLoader
-
-URL = r"https://raw.githubusercontent.com/zhukovsd/java-backend-interview-prep/refs/heads/master/data/%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D1%8B%20Java/Collections.toml"
-DESTINATION = r"C:/Users/user/PycharmProjects/Toml2Anki/Test"
-PICTURE_URL = r"/java-backend-interview-prep/hibernate_entity_life_cycle.png"
-PICTURES_SOURCE = r"https://zhukovsd.github.io"
 
 def print_hi(name):
     print(f'Hi, {name}')
@@ -12,5 +9,12 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    toml = TomlLoader.get_from_url(URL)
-    DeckGenerator.generate_deck(DESTINATION, toml)
+    parser = argparse.ArgumentParser(description='Пример скрипта для чтения аргументов командной строки.')
+
+    parser.add_argument('url', type=str, help='Ссылка на TOML, из репозитория')
+    parser.add_argument('path', type=str, help='Папка, куда будет сохранена колода')
+
+    args = parser.parse_args()
+
+    toml = TomlLoader.get_from_url(args.url)
+    DeckGenerator.generate_deck(args.path, toml)
